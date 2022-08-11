21339c318a7dcdc91645f072ef401f8e3e59d5ad
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-11 18:58:52 -0400

    Add strings and typography to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Typography } from '@mui/material';
+import { strings } from './constants/strings';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Typography variant='h1'>{strings.HEY}</Typography>
+      <Typography variant='body1'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
+    </>
   );
 }
 
