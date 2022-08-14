e8e7020567551e4967943668133eed2c77514b66
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 17:14:39 -0400

    Fix theme TS errors

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -1,11 +1,23 @@
-// @ts-nocheck TODO: Fix type errors
 import { createTheme } from '@mui/material';
 
+declare module '@mui/material/styles' {
+    interface TypographyVariants {
+        bodyCode: React.CSSProperties;
+    }
+
+    // allow configuration using `createTheme`
+    interface TypographyVariantsOptions {
+        bodyCode?: React.CSSProperties;
+    }
+}
+
+// Update the Typography's variant prop options
 declare module '@mui/material/Typography' {
     interface TypographyPropsVariantOverrides {
         bodyCode: true;
     }
 }
+
 
 export const theme = createTheme({
     typography: {
