252dd71caefe3fe6e3eeb666760628475e49e5f2
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 17:43:54 -0400

    Add Background

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import { strings } from './constants/strings';
 import { MarginLayout } from './components/layout';
 import Headshot from './assets/images/headshot.jpg';
 
+const Background = styled('div')`
+    position: absolute;
+    background-color: #F20587;
+    background-image: -webkit-linear-gradient(30deg, #F20587 50%, #033E8C 50%);
+    min-height: 100%;
+`;
+
 const Row = styled('div')`
     display: flex;
     flex-direction: row;
@@ -21,55 +28,58 @@ const Column = styled('div')`
 
 const App = () => {
   return (
-    <MarginLayout>
-      <Box
-        component="img"
-        sx={{
-          width: 350,
-          // height: 233,
-          // maxHeight: { xs: 233, md: 167 },
-          // maxWidth: { xs: 350, md: 250 },
-        }}
-        alt="Jamie McNeil Headshot"
-        src={Headshot}
-      />
+    <Background>
+      <MarginLayout>
+        <Box
+          component="img"
+          sx={{
+            width: 350,
+            // boxShadow: '-50px -50px 0 -40px red, 50px 50px 0 -40px red'
+            // height: 233,
+            // maxHeight: { xs: 233, md: 167 },
+            // maxWidth: { xs: 350, md: 250 },
+          }}
+          alt="Jamie McNeil Headshot"
+          src={Headshot}
+        />
 
-      <Row>
-        <Column>
-          <Typography variant='h1'>{strings.HEY}</Typography>
-          <Typography variant='bodyCode'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
-        </Column>
+        <Row>
+          <Column>
+            <Typography variant='h1'>{strings.HEY}</Typography>
+            <Typography variant='bodyCode'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
+          </Column>
 
-        <Column>
-          <Button variant="outlined">{strings.GET_IN_TOUCH}</Button>
-        </Column>
-      </Row>
+          <Column>
+            <Button variant="outlined">{strings.GET_IN_TOUCH}</Button>
+          </Column>
+        </Row>
 
-      <Row>
-        <Column>
-          <Typography variant='bodyCode'>{strings.ABOUT}</Typography>
-        </Column>
+        <Row>
+          <Column>
+            <Typography variant='bodyCode'>{strings.ABOUT}</Typography>
+          </Column>
 
-        <Column>
-          <Typography variant='bodyCode'>{strings.EXPERIENCE}</Typography>
-        </Column>
-      </Row>
+          <Column>
+            <Typography variant='bodyCode'>{strings.EXPERIENCE}</Typography>
+          </Column>
+        </Row>
 
-      <Row>
-        <Column>
-          <Link href='https://www.linkedin.com/in/jamiemcneil/' target="_blank" rel="noopener"><Typography variant='body1'>LinkedIn</Typography></Link>
-        </Column>
+        <Row>
+          <Column>
+            <Link href='https://www.linkedin.com/in/jamiemcneil/' target="_blank" rel="noopener"><Typography variant='body1'>LinkedIn</Typography></Link>
+          </Column>
 
-        <Column>
-          <Link href='https://github.com/jvmcneil' target="_blank" rel="noopener"><Typography variant='body1'>GitHub</Typography></Link>
-        </Column>
+          <Column>
+            <Link href='https://github.com/jvmcneil' target="_blank" rel="noopener"><Typography variant='body1'>GitHub</Typography></Link>
+          </Column>
 
-        <Column>
-          <Link href='https://www.instagram.com/radicalnetrunner/' target="_blank" rel="noopener"><Typography variant='body1'>Instagram</Typography></Link>
-        </Column>
-      </Row>
+          <Column>
+            <Link href='https://www.instagram.com/radicalnetrunner/' target="_blank" rel="noopener"><Typography variant='body1'>Instagram</Typography></Link>
+          </Column>
+        </Row>
 
-    </MarginLayout>
+      </MarginLayout>
+    </Background>
   );
 }
 
