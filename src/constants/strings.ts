853b30d6e1540af60d7e878c81eeb4ecf92eb759
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 14:38:22 -0400

    Add ABOUT string

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,4 +1,5 @@
 export const strings = {
     HEY: 'Hey, I\'m Jamie',
     FRONTEND_SOFTWARE_DEVELOPER: 'Front-End Software Developer and Lover of Sushi 🍣',
+    ABOUT: 'I am an experienced React developer with a deep understanding of UI design, state management, and mobile applications. My goal for every project I work on is writing clean, bug free code that presents a great experience for the end user.',
 }
