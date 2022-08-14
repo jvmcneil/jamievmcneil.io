54166b1a4008c61f11e97ab618095090cf03e1d5
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 16:01:23 -0400

    Adjust ABOUT string; Add EXPERIENCE string

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -2,5 +2,6 @@ export const strings = {
     HEY: 'Hey, I\'m Jamie',
     FRONTEND_SOFTWARE_DEVELOPER: 'Front-End Software Developer and Lover of Sushi 🍣',
     GET_IN_TOUCH: 'Get In Touch',
-    ABOUT: 'I am an experienced React developer with a deep understanding of UI design, state management, and mobile applications. I have worked on projects for global companies such as Alcon and Holland America Line. My goal for every project I work on is writing clean, bug free code that presents a great experience for the end user.',
+    ABOUT: 'I am an experienced React developer with a deep understanding of UI design, state management, and mobile applications.',
+    EXPERIENCE: 'I have worked on successful projects for global companies such as Alcon and Holland America Line.'
 }
