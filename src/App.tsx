c2b705e669b209f0e9566eb5571af40393eef544
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 15:29:23 -0400

    Add flex container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Button, Typography, styled } from '@mui/material';
 import { strings } from './constants/strings';
 import { MarginLayout } from './components/layout';
+
+const Content = styled('div')`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 2rem;
+
+  >* {
+    width: 45%;
+  }
+`;
 
 const App = () => {
   return (
     <MarginLayout>
       <Typography variant='h1'>{strings.HEY}</Typography>
       <Typography variant='body1'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
-      <Typography variant='body1'>{strings.ABOUT}</Typography>
+      <Button variant="outlined">{strings.GET_IN_TOUCH}</Button>
+
+      <Content>
+        <Typography variant='body1'>{strings.ABOUT}</Typography>
+      </Content>
+
     </MarginLayout>
   );
 }
