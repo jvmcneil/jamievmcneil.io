2f8417f81d733d70f267b7ef32d158f885762d2c
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 14:38:33 -0400

    Add ABOUT string to page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
     <MarginLayout>
       <Typography variant='h1'>{strings.HEY}</Typography>
       <Typography variant='body1'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
+      <Typography variant='body1'>{strings.ABOUT}</Typography>
     </MarginLayout>
   );
 }
