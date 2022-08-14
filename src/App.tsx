990e709fef739a85a45c1b7291c3d2cbe00b0fcc
Jamie McNeil <jamievmcneil@gmail.com>
2022-08-14 15:53:34 -0400

    Add Headshot to page; Make use of MUI Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Button, Typography, styled } from '@mui/material';
+import { Box, Button, Typography, styled } from '@mui/material';
 import { strings } from './constants/strings';
 import { MarginLayout } from './components/layout';
+import Headshot from './assets/images/headshot.jpg';
 
 const Content = styled('div')`
   display: flex;
@@ -17,6 +18,18 @@ const Content = styled('div')`
 const App = () => {
   return (
     <MarginLayout>
+      <Box
+        component="img"
+        sx={{
+          width: 350,
+          // height: 233,
+          // maxHeight: { xs: 233, md: 167 },
+          // maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="Jamie McNeil Headshot"
+        src={Headshot}
+      />
+
       <Typography variant='h1'>{strings.HEY}</Typography>
       <Typography variant='body1'>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
       <Button variant="outlined">{strings.GET_IN_TOUCH}</Button>
