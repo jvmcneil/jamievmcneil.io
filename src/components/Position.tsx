57fc0df7f8462cf1f39c2158edb1aeed2969509c
Jamie McNeil <jamievmcneil@gmail.com>
2024-02-02 18:44:46 -0500

    Add Position component

---
+++ src/components/Position.tsx
@@ -0,0 +1,34 @@
+import { Chip, Stack, Typography } from '@mui/material';
+import { PositionContainer } from './Styles.css';
+
+interface PositionProps {
+  title: string;
+  dates?: string;
+  location: string;
+  description: string;
+  skills: string[];
+}
+
+const Position = ({ title, dates, location, description, skills }: PositionProps) => (
+  <PositionContainer>
+    <Typography variant="h6" component="h3">
+      {title}
+    </Typography>
+    <Typography variant="subtitle2" component="p">
+      {dates}
+    </Typography>
+    <Typography variant="subtitle2" component="p">
+      {location}
+    </Typography>
+    <Typography variant="body1" component="p">
+      {description}
+    </Typography>
+    <Stack direction="row" spacing={1}>
+      {skills.map((skill) => (
+        <Chip label={skill} color="primary" size="small" />
+      ))}
+    </Stack>
+  </PositionContainer>
+);
+
+export default Position;
