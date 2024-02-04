f73e14e7474d47b67e10a8e8d580a12de7752d64
Jamie McNeil <jamievmcneil@gmail.com>
2024-02-04 11:26:26 -0500

    Move ThemeProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Box, Typography } from '@mui/material';
+import { Box, ThemeProvider, Typography } from '@mui/material';
 import Headshot from './assets/images/headshot.jpg';
 import { strings } from './constants/strings';
+import { theme } from './constants/theme';
 import { MarginLayout } from './components/layout';
 import { Background, HeadshotContainer } from './components/Styles.css';
 import WorkExperience from './components/WorkExperience';
@@ -9,35 +10,37 @@ import Footer from './components/Footer';
 import SideProjects from './components/SideProjects';
 
 const App = () => (
-  <Background>
-    <MarginLayout>
-      <Typography variant="h3" component="h1">
-        {strings.HELLO}
-      </Typography>
-
-      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <HeadshotContainer
-          alt="Jamie McNeil Headshot"
-          src={Headshot}
-          sx={{ width: 80, height: 80 }}
-        />
-
-        <Typography sx={{ ml: '0.875rem' }}>
-          <p>{strings.FRONTEND_SOFTWARE_DEVELOPER}</p>
-
-          <p>{strings.ABOUT}</p>
+  <ThemeProvider theme={theme}>
+    <Background>
+      <MarginLayout>
+        <Typography variant="h3" component="h1">
+          {strings.HELLO}
         </Typography>
-      </Box>
 
-      <WorkExperience />
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <HeadshotContainer
+            alt="Jamie McNeil Headshot"
+            src={Headshot}
+            sx={{ width: 80, height: 80 }}
+          />
 
-      <Education />
+          <Typography sx={{ ml: '0.875rem' }}>
+            <p>{strings.FRONTEND_SOFTWARE_DEVELOPER}</p>
 
-      <SideProjects />
+            <p>{strings.ABOUT}</p>
+          </Typography>
+        </Box>
 
-      <Footer />
-    </MarginLayout>
-  </Background>
+        <WorkExperience />
+
+        <Education />
+
+        <SideProjects />
+
+        <Footer />
+      </MarginLayout>
+    </Background>
+  </ThemeProvider>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './constants/theme';
-import './index.css';
+ThemeProviderimport './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
