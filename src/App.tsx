2d00e44cca4d06937ce69604aac072c5f769072f
Jamie McNeil <jamievmcneil@gmail.com>
2024-02-05 23:13:44 -0500

    Set global style for bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Box, Typography, useTheme } from '@mui/material';
+import { Box, GlobalStyles, Typography, useTheme } from '@mui/material';
 import Headshot from './assets/images/headshot.jpg';
 import { strings } from './constants/strings';
 import { MarginLayout } from './components/layout';
@@ -19,45 +19,45 @@ import SideProjects from './components/SideProjects';
 const App = () => {
   const theme = useTheme();
   const themeContext = useContext(ThemeWrapperContext);
+  const backgroundColor = theme?.palette?.background?.default;
 
   return (
-    <Box>
-      <Background sx={{ bgcolor: 'background.default', color: 'text.primary' }}>
-        <MarginLayout>
-          <NameContainer>
-            <Typography variant="h3" component="h1">
-              {strings.HELLO}
-            </Typography>
-            <ThemeSwitch
-              onClick={() => themeContext.toggleColorMode()}
-              checked={theme?.palette?.mode === 'dark'}
-            />
-          </NameContainer>
+    <Background sx={{ color: 'text.primary' }}>
+      <GlobalStyles styles={{ body: { backgroundColor } }} />
+      <MarginLayout>
+        <NameContainer>
+          <Typography variant="h3" component="h1">
+            {strings.HELLO}
+          </Typography>
+          <ThemeSwitch
+            onClick={() => themeContext.toggleColorMode()}
+            checked={theme?.palette?.mode === 'dark'}
+          />
+        </NameContainer>
 
-          <HeadshotIntroContainer>
-            <HeadshotContainer
-              alt="Jamie McNeil Headshot"
-              src={Headshot}
-              sx={{ width: 80, height: 80 }}
-            />
+        <HeadshotIntroContainer>
+          <HeadshotContainer
+            alt="Jamie McNeil Headshot"
+            src={Headshot}
+            sx={{ width: 80, height: 80 }}
+          />
 
-            <Box sx={{ my: 2 }}>
-              <Typography sx={{ mb: 2 }}>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
+          <Box sx={{ my: 2 }}>
+            <Typography sx={{ mb: 2 }}>{strings.FRONTEND_SOFTWARE_DEVELOPER}</Typography>
 
-              <Typography>{strings.ABOUT}</Typography>
-            </Box>
-          </HeadshotIntroContainer>
+            <Typography>{strings.ABOUT}</Typography>
+          </Box>
+        </HeadshotIntroContainer>
 
-          <WorkExperience />
+        <WorkExperience />
 
-          <Education />
+        <Education />
 
-          <SideProjects />
+        <SideProjects />
 
-          <Footer />
-        </MarginLayout>
-      </Background>
-    </Box>
+        <Footer />
+      </MarginLayout>
+    </Background>
   );
 };
 
